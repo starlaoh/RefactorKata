41a8078c68443cb562986be8ebd4f56a250d5e11
Starla Oden <starlaoh@gmail.com>
2018-01-24 09:22:54 -0800

    Refactored Name variable in Product class

--- RefactorKata/Program.cs
+++ RefactorKata/Program.cs
@@ -19,7 +19,7 @@ namespace RefactorKata
             //TODO: Replace with Dapper
             while (reader.Read())
             {
-                var prod = new Product { name = reader["Name"].ToString() };
+                var prod = new Product { Name = reader["Name"].ToString() };
                 products.Add(prod);
             }
 
@@ -27,7 +27,7 @@ namespace RefactorKata
 
             foreach(var product in products)
             {
-                Console.WriteLine(product.name);
+                Console.WriteLine(product.Name);
             }
         }
     }
